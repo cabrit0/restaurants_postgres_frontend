d27031248a3644b395e01ece27975bb74debb514
cabrit0 <cabrit0o.dev@gmail.com>
2022-06-16 16:31:51 +0100

    VARS

--- src/api/RestaurantFinder.js
+++ src/api/RestaurantFinder.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://cryptic-fortress-14262.herokuapp.com//api/v1/restaurants",
+  baseURL: "https://cryptic-fortress-14262.herokuapp.com/api/v1/restaurants",
 });
